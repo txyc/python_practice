40f8a7b444abd340931cd2c743ea05cbb6eae251
txyc <965622832@qq.com>
2023-04-30 22:09:03 +0800

    增加list函数的测试

--- test_list_set_dict.py
+++ test_list_set_dict.py
@@ -1,3 +1,46 @@
+# list
+print("----next step is python list test----")
+ii = [1,2,3,4,5]
+print(type(ii))
+print(isinstance(ii,list))
+ii[2] = 5
+print(ii)
+ii.remove(2)
+print(ii)
+ii.pop(1)
+print(ii)
+del ii[1]
+print(ii)
+ii.clear()
+print(ii)
+ii.extend("aa")
+print(ii)
+ii.append("b")
+print(ii.count("a"),ii.count("b"))
+print(ii.index("a"))
+ii.reverse()
+print(ii)
+ii.sort()
+print(ii)
+ii.sort(reverse=True)
+print(ii)
+ii.insert()
+# tuple
+print("----next step is python tuple test----")
+jj = (1,2,3,4,5)
+print(type(jj))
+print(isinstance(jj,tuple))
+# jj[2] = 5 # unable to edit tuple
+print(jj)
+# set
+print("----next step is python set test----")
+kk = {1,2,3,4,5}
+print(type(kk))
+print(isinstance(kk,set))
+#kk[2] = 4 # unable to edit set
+print(kk)
+# dict
+print("----next step is python dict test----")
 a = {}
 print(type(a))
 print(isinstance(a,dict))
