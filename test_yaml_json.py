7dcb61a86ef5b6b8d8c92c5496aa61145915f3c3
txyc <965622832@qq.com>
2023-05-14 20:35:15 +0800

    补充yaml和json的使用

---
+++ test_yaml_json.py
@@ -0,0 +1,26 @@
+import yaml
+import json
+import os
+
+if __name__ == "__main__":
+    data_test = {"name":"Jack","age":25,"job":"teacher"}
+
+    config_dir = os.sep.join([os.path.dirname(__file__), "config"])
+    if not os.path.exists(config_dir):
+        os.mkdir(config_dir)
+
+    yaml_path = os.sep.join([config_dir,"yamldata.yml"])
+    f = open(yaml_path,"w")
+    yaml.dump(data_test,f)
+    f.close()
+
+    f = open(yaml_path,"r")
+    data_test1 = yaml.safe_load(f)
+    f.close()
+    print(data_test1)
+
+    json_path = os.sep.join([config_dir,"jsondata.json"])
+    f = open(json_path,"rb")
+    data_test2 = json.loads(f)
+    f.close()
+    print(data_test2)
