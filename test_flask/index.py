1a7f82e8999ccf8144561c2317e79aab867f5b66
txyc <965622832@qq.com>
2023-06-13 21:24:33 +0800

    补充flask配合数据库的呈现

--- test_flask/index.py
+++ test_flask/index.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Tianxin_2015@localhost:3306/test_userinfo' # 连接到您的数据库
 db = SQLAlchemy(app)
 
-class userinfo(db.Model):
+class Userinfo(db.Model):
+    __tablename__ = 'userinfo'
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(50), nullable=False)
     contact_info = db.Column(db.String(200), nullable=False)
@@ -26,6 +27,11 @@ def page2():
 def add_info_form():
     return render_template('add_info_form.html')
 
+@app.route('/display_sql_form')
+def display_sql_form():
+    users = Userinfo.query.all()
+    return render_template('display_sql_form.html', users=users)
+
 @app.route('/back')
 def back():
     return redirect('/')  # 返回主页
@@ -36,7 +42,7 @@ def submit():
     name = request.form.get('name')
     contact_info = request.form.get('contact_info')
 
-    contact = userinfo(id=6, user=name, contact_info=contact_info)
+    contact = Userinfo(id=5, user=name, contact_info=contact_info)
     db.session.add(contact)  # 将新的test_userinfo实例添加到会话中以进行持久化操作
     db.session.commit()  # 提交更改到数据库
 
